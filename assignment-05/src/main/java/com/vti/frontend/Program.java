a597ef378cb886b98a4fb49a716b88895d654e49
Heenino <s2luv.kunky@gmail.com>
2022-12-07 21:36:31 +0900

    Assignment_05

---
+++ assignment-05/src/main/java/com/vti/frontend/Program.java
@@ -0,0 +1,12 @@
+package com.vti.frontend;
+
+import com.vti.backend.Execise01;
+
+import java.util.Scanner;
+
+public class Program {
+    Scanner scanner =new Scanner(System.in);
+    public static void main(String[] args) {
+        Execise01.question01();
+    }
+}
